2cd043a1d89602b9168360f497a9cc04b598d308
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-31 06:37:54 -0300

    implementing fitness function algorithm

--- ch02-algoritimos-de-otimização/dados-voos.py
+++ ch02-algoritimos-de-otimização/dados-voos.py
@@ -1,3 +1,6 @@
+import mlrose
+
+
 pessoas = [('Lisboa', 'LIS'),
            ('Madrid', 'MAD'),
            ('Paris', 'CDG'),
@@ -145,4 +148,23 @@ def imprimir_voos(agenda):
                                                     volta[0], volta[1], volta[2]))
     print('Preço total: ', total_preco)
 
+
+def fitness_function(agenda):
+    id_voo = -1
+    total_preco = 0
+    for i in range(len(agenda) // 2):
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][agenda[id_voo]]
+        total_preco += ida[2]
+        id_voo += 1
+        volta = voos[(destino, origem)][agenda[id_voo]]
+        total_preco += volta[2]
+    
+    return total_preco
+
+
+fitness = mlrose.CustomFitness(fitness_function)
+
+problema = mlrose.DiscreteOpt(length=12, fitness_fn=fitness, maximize=False, max_val=10)
 

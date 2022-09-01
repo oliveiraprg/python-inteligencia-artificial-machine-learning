a4fab13eae398b21776aefecaedc5e28cb119dbc
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-09-01 19:54:55 -0300

    implementing other functions for case study

--- ch03-logica-difusa-fuzzy/exemplo_gorjetas.py
+++ ch03-logica-difusa-fuzzy/exemplo_gorjetas.py
@@ -1,6 +1,7 @@
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
+import random
 
 
 #Antecendentes e Consequente
@@ -12,10 +13,10 @@ gorjeta = ctrl.Consequent(np.arange(0,21, 1), 'gorjeta')
 qualidade.automf(number=3, names=['ruim', 'boa', 'saborosa'])
 servico.automf(number=3, names=['ruim', 'aceitável', 'ótimo'])
 
-#Consequente - Solução baseada em forma triangular
-gorjeta['baixa'] = fuzz.trimf(gorjeta.universe, [0, 0, 5])
-gorjeta['media'] = fuzz.trimf(gorjeta.universe, [2, 10, 15])
-gorjeta['alta'] = fuzz.trimf(gorjeta.universe, [15, 18, 20])
+#Consequente - Solução baseada na forma sigmoid
+gorjeta['baixa'] = fuzz.sigmf(gorjeta.universe, 5, -1)
+gorjeta['media'] = fuzz.gaussmf(gorjeta.universe, 10, 3)
+gorjeta['alta'] = fuzz.pimf(gorjeta.universe, 10, 20, 20, 21)
 
 #Regras de referencia, podendo ter uma ou mais regras
 regra1 = ctrl.Rule(qualidade['ruim'] | servico['ruim'], gorjeta['baixa'])
@@ -25,7 +26,10 @@ regra3 = ctrl.Rule(qualidade['saborosa'] | servico['ótimo'], gorjeta['alta'])
 #Sistema de Controle
 sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3])
 sistema = ctrl.ControlSystemSimulation(sistema_controle)
-sistema.input['qualidade'] = 10
-sistema.input['servico'] = 10
+nota_servico = random.randint(0, 11)
+nota_qualidade = random.randint(0, 11)
+sistema.input['qualidade'] = nota_qualidade
+sistema.input['servico'] = nota_servico
 sistema.compute()
-print(sistema.output['gorjeta'])
+valor_gorgeta = sistema.output['gorjeta']
+print(f'Nota dada ao serviço: {nota_servico}, Nota dada a qualidade da comida {nota_qualidade}, Valor de gorjeta indicado {round(valor_gorgeta, 2)}')
